2a397fe763afd5392e1a8c38bd124fcea773ed59
Vinayaga Sundar <vinayaga.sundar@tarento.com>
2017-04-13 19:52:01 +0530

    added option to display user has taken too many attempts.

--- FingerPrintSample/app/src/main/java/com/loviraj/fingerprintsample/auth/FPStatusListener.java
+++ FingerPrintSample/app/src/main/java/com/loviraj/fingerprintsample/auth/FPStatusListener.java
@@ -8,4 +8,12 @@ public interface FPStatusListener {
     void fpAuthSuccess();
 
     void fpAuthFailed();
+
+    /**
+     * If there is any problem in authenticate using fingerprint scanner.
+     * it'll be called
+     * Like if you exceed more number of attempts.
+     * @param error error message from the fingerprint auth.
+     */
+    void fpAuthError(String error);
 }

b8afc720433afe8135c1f99589e526ad66234ace
rmkrishna <rmkrishna.sacoe@gmail.com>
2017-06-13 11:00:36 +0530

    Added MeiZu Device support.

--- FingerPrintSample/app/src/main/java/com/loviraj/fingerprintsample/auth/LFingerPrint.java
+++ FingerPrintSample/app/src/main/java/com/loviraj/fingerprintsample/auth/LFingerPrint.java
@@ -2,6 +2,7 @@ package com.loviraj.fingerprintsample.auth;
 
 import android.content.Context;
 import android.os.Build;
+import android.text.TextUtils;
 import android.util.Log;
 
 /**
@@ -10,21 +11,30 @@ import android.util.Log;
 
 public class LFingerPrint {
     private static final String TAG = LFingerPrint.class.getSimpleName();
-    
+
     private LFPManager mLFingerPrintManager;
 
     public LFingerPrint(Context context, FPStatusListener listener) {
 
         final boolean isSamsungDevice = isSamsungDevice(Build.MANUFACTURER, Build.BRAND);
+        final boolean isMeiZuDevice = isMeiZuDevice(Build.MANUFACTURER);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if (Build.VERSION.SDK_INT >= 23) { // 23 -> Build.VERSION_CODES.M
             Log.d(TAG, "LFingerPrint: " + Build.VERSION_CODES.M);
             mLFingerPrintManager = new MarshmallowFP(context, listener);
 
-            if (!mLFingerPrintManager.isFingerPrintAvailable() && isSamsungDevice) {
-                Log.d(TAG, "LFingerPrint: samsung");
-                mLFingerPrintManager = new SamsungPassFP(context, listener);
+            if (!mLFingerPrintManager.isFingerPrintAvailable()) {
+                if (isSamsungDevice) {
+                    Log.d(TAG, "LFingerPrint: samsung");
+                    mLFingerPrintManager = new SamsungPassFP(context, listener);
+                } else if (isMeiZuDevice) {
+                    Log.d(TAG, "LFingerPrint: MeiZuDevice");
+                    mLFingerPrintManager = new MeiZuFP(context, listener);
+                }
             }
+        } else if (Build.VERSION.SDK_INT >= 21 && isMeiZuDevice) { // // 23 -> Build.VERSION_CODES.L
+            Log.d(TAG, "LFingerPrint: MeiZuDevice");
+            mLFingerPrintManager = new MeiZuFP(context, listener);
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 && isSamsungDevice) {
             Log.d(TAG, "LFingerPrint: samsung");
             mLFingerPrintManager = new SamsungPassFP(context, listener);
@@ -41,5 +51,9 @@ public class LFingerPrint {
         return (strBrand != null && strManufacturer != null)
                 && (strBrand.compareToIgnoreCase("Samsung") == 0
                 || strManufacturer.compareToIgnoreCase("Samsung") == 0);
+    }
+
+    private static boolean isMeiZuDevice(String strManufacturer) {
+        return !TextUtils.isEmpty(strManufacturer) && strManufacturer.toLowerCase().contains("meizu");
     }
 }
